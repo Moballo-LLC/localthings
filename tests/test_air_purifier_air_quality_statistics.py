258cdc16472f1bb3c6d53dc342f677a0b826f411
JayChickenK <19538983+JayChickenK@users.noreply.github.com>
2026-08-18 11:07:51 +0200

    tests: apply ruff format to CO2 assertions

--- tests/test_air_purifier_air_quality_statistics.py
+++ tests/test_air_purifier_air_quality_statistics.py
@@ -110,9 +110,9 @@ def test_co2_matches_air_monitor_mapping():
     assert desc.unit == "ppm"
     assert desc.state_class == "measurement"
     assert desc.exists_fn is not None
-    assert desc.value_fn(
-        [{"x.com.samsung.da.type": "CO2", "x.com.samsung.da.value": ["498"]}]
-    ) == 498
+    assert (
+        desc.value_fn([{"x.com.samsung.da.type": "CO2", "x.com.samsung.da.value": ["498"]}]) == 498
+    )
 
 
 def test_every_air_quality_sensor_still_reads_a_plain_int():
